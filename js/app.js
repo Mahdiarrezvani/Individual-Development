5058d52ccf1c0408fade99b771a7178b17bbb063
mahdiar <mahdiarrezvani59@gmail.com>
2023-02-13 19:43:15 -0800

    update data base

--- js/app.js
+++ js/app.js
@@ -6,30 +6,36 @@ let userBtn = [
 let dataBase = [
     // Mahdiar
     mahdiar = [
-        { date: "0000/00/00", day: "---", lesson: "60", code: "75", wasted: "---", study: "", english: "" },
-        { date: "0000/00/00", day: "---", lesson: "150", code: "85", wasted: "---", study: "", english: "" },
-        { date: "1401/11/16", day: "sunday", lesson: "0", code: "140", wasted: "90", study: "", english: "" },
+        { date: "1401/11/16", day: "sunday", lesson: "0", code: "140", wasted: "90", study: "0", english: "0" },
         { date: "1401/11/17", day: "monday", lesson: "0", code: "200", wasted: "135", study: "0", english: "0" },
         { date: "1401/11/18", day: "tuesday", lesson: "0", code: "120", wasted: "40", study: "23", english: "23" },
         { date: "1401/11/19", day: "wendesday", lesson: "90", code: "144", wasted: "120", study: "0", english: "27" },
+        { date: "1401/11/20", day: "thursday", lesson: "93", code: "90", wasted: "120", study: "15", english: "39" },
+        { date: "1401/11/21", day: "friday", lesson: "0", code: "240", wasted: "150", study: "25", english: "40" },
+        { date: "1401/11/22", day: "saturday", lesson: "113", code: "0", wasted: "200", study: "25", english: "0" },
+        { date: "1401/11/23", day: "sunday", lesson: "110", code: "0", wasted: "120", study: "13", english: "0" },
     ],
     // Amin
     amin = [
-        { date: "0000/00/00", day: "---", lesson: "0", code: "180", wasted: "---", study: "", english: "" },
-        { date: "0000/00/00", day: "---", lesson: "150", code: "300", wasted: "---", study: "", english: "" },
-        { date: "1401/11/16", day: "sunday", lesson: "0", code: "240", wasted: "100", study: "", english: "" },
+        { date: "1401/11/16", day: "sunday", lesson: "0", code: "240", wasted: "100", study: "0", english: "0" },
         { date: "1401/11/17", day: "monday", lesson: "0", code: "150", wasted: "150", study: "0", english: "60" },
         { date: "1401/11/18", day: "tuesday", lesson: "150", code: "20", wasted: "60", study: "0", english: "0" },
         { date: "1401/11/19", day: "wendesday", lesson: "90", code: "120", wasted: "40", study: "0", english: "60" },
+        { date: "1401/11/20", day: "thursday", lesson: "0", code: "120", wasted: "0", study: "0", english: "0" },
+        { date: "1401/11/21", day: "friday", lesson: "0", code: "0", wasted: "0", study: "0", english: "0" },
+        { date: "1401/11/22", day: "saturday", lesson: "150", code: "75", wasted: "120", study: "0", english: "0" },
+        { date: "1401/11/23", day: "sunday", lesson: "120", code: "30", wasted: "120", study: "30", english: "0" },
     ],
     // Amir Abbas
     abbas = [
-        { date: "0000/00/00", day: "---", lesson: "0", code: "180", wasted: "---", study: "", english: "" },
-        { date: "0000/00/00", day: "---", lesson: "---", code: "0", wasted: "---", study: "", english: "" },
-        { date: "1401/11/16", day: "sunday", lesson: "0", code: "60", wasted: "300", study: "", english: "" },
+        { date: "1401/11/16", day: "sunday", lesson: "0", code: "60", wasted: "300", study: "0", english: "0" },
         { date: "1401/11/17", day: "monday", lesson: "30", code: "0", wasted: "300", study: "0", english: "0" },
         { date: "1401/11/18", day: "tuesday", lesson: "120", code: "240", wasted: "90", study: "0", english: "0" },
         { date: "1401/11/19", day: "wendesday", lesson: "120", code: "0", wasted: "100", study: "20", english: "15" },
+        { date: "1401/11/19", day: "thursday", lesson: "60", code: "219", wasted: "180", study: "0", english: "0" },
+        { date: "1401/11/19", day: "friday", lesson: "0", code: "0", wasted: "300", study: "0", english: "0" },
+        { date: "1401/11/22", day: "saturday", lesson: "120", code: "0", wasted: "300", study: "0", english: "0" },
+        { date: "1401/11/23", day: "sunday", lesson: "300", code: "30", wasted: "120", study: "0", english: "0" },
     ],
 ];
 let btnContainer = document.querySelector('.btn');
