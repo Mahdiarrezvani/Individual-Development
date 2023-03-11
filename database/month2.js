86b2341d38ffe7ba2d2436f12c5091c7ae702c74
mahdiar <mahdiarrezvani59@gmail.com>
2023-03-11 12:52:39 -0800

    dataBase month 1 and 2

---
+++ database/month2.js
@@ -0,0 +1,19 @@
+let month2Mahdiar = [
+    { date: "1401/12/16", day: "tuesday", lesson: "110", code: "101", wasted: "210", study: "13", english: "0", learning: '0' },
+    { date: "1401/12/17", day: "wendesday", lesson: "58", code: "131", wasted: "160", study: "0", english: "24", learning: '0' },
+    { date: "1401/12/18", day: "thursday", lesson: "76", code: "128", wasted: "145", study: "0", english: "25", learning: '46' },
+    { date: "1401/12/19", day: "friday", lesson: "58", code: "196", wasted: "120", study: "0", english: "39", learning: '26' },
+];
+let month2Amin = [
+    { date: "1401/12/16", day: "tuesday", lesson: "0", code: "91", wasted: "180", study: "0", english: "0", learning: '0' },
+    { date: "1401/12/17", day: "wendesday", lesson: "90", code: "84", wasted: "180", study: "0", english: "0", learning: '0' },
+    { date: "1401/12/18", day: "thursday", lesson: "0", code: "0", wasted: "0", study: "0", english: "0", learning: '0' },
+    { date: "1401/12/19", day: "friday", lesson: "30", code: "0", wasted: "60", study: "0", english: "0", learning: '0' },
+];
+let month2Abbas = [
+    { date: "1401/12/16", day: "tuesday", lesson: "0", code: "0", wasted: "360", study: "0", english: "0", learning: '0' },
+    { date: "1401/12/17", day: "wendesday", lesson: "80", code: "180", wasted: "120", study: "10", english: "0", learning: '30' },
+    { date: "1401/12/18", day: "thursday", lesson: "60", code: "150", wasted: "180", study: "20", english: "0", learning: '0' },
+    { date: "1401/12/19", day: "friday", lesson: "0", code: "90", wasted: "240", study: "0", english: "0", learning: '16' },
+];
+export { month2Mahdiar, month2Amin, month2Abbas };
