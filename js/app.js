a9a25d0981a5cb78ee285237da9a3dbf079e58db
mahdiar <mahdiarrezvani59@gmail.com>
2023-03-14 18:59:09 -0700

    sfdujh

--- js/app.js
+++ js/app.js
@@ -8,27 +8,27 @@ function workMofid() {
     let sumMahdiar = 0;
     let sumAmin = 0;
     let sumAbbas = 0;
-    dataBase[0].forEach(function(e) {
-        subjectsMofid.forEach(function(subject) {
+    dataBase[0].forEach(function (e) {
+        subjectsMofid.forEach(function (subject) {
             sumMahdiar = sumMahdiar + +e[subject];
         });
     });
-    dataBase[1].forEach(function(e) {
-        subjectsMofid.forEach(function(subject) {
+    dataBase[1].forEach(function (e) {
+        subjectsMofid.forEach(function (subject) {
             sumAmin = sumAmin + +e[subject];
         });
     });
-    dataBase[2].forEach(function(e) {
-        subjectsMofid.forEach(function(subject) {
+    dataBase[2].forEach(function (e) {
+        subjectsMofid.forEach(function (subject) {
             sumAbbas = sumAbbas + +e[subject];
         });
     });
     mofid.innerHTML = `
-    <p class="title">کار های مفید</p>
+    <p class="title">useful time</p>
     <div>
-        <p>mahdiar : <span>${sumMahdiar}</span></p>
-        <p>amin : <span>${sumAmin}</span></p>
-        <p>amir abbas : <span>${sumAbbas}</span></p>
+        <p>mahdiar: <span>${sumMahdiar}</span></p>
+        <p>amin: <span>${sumAmin}</span></p>
+        <p>amir abbas: <span>${sumAbbas}</span></p>
     </div>
     `;
 }
@@ -41,21 +41,21 @@ let sumAmin = 0;
 let sumAbbas = 0;
 
 function first(subject) {
-    dataBase[0].forEach(function(e) {
+    dataBase[0].forEach(function (e) {
         sumMahdiar = sumMahdiar + +e[subject];
     });
     // 
-    dataBase[1].forEach(function(e) {
+    dataBase[1].forEach(function (e) {
         sumAmin = sumAmin + +e[subject];
     });
     // 
-    dataBase[2].forEach(function(e) {
+    dataBase[2].forEach(function (e) {
         sumAbbas = +sumAbbas + +e[subject];
     });
     // 
     let subjects = [sumAbbas, sumAmin, sumMahdiar];
     let firstPerson = null;
-    (function() {
+    (function () {
         // first Person
         let bigger = null;
         if (subject == 'wasted') {
@@ -64,7 +64,7 @@ function first(subject) {
             bigger = Math.max(sumAbbas, sumAmin, sumMahdiar);
         }
         let firstPersonNumber = null;
-        subjects.forEach(function(e) {
+        subjects.forEach(function (e) {
             if (e == bigger) {
                 return firstPersonNumber = e;
             }
@@ -76,7 +76,7 @@ function first(subject) {
     <p>${subject} : <span>${firstPerson}</span></p>
     `);
 }
-subjects.forEach(function(subject) {
+subjects.forEach(function (subject) {
     first(subject);
 });
 
