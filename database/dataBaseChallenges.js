26f641e636e94878bec85f928e443d40cc1a2ffc
mahdiar <mahdiarrezvani59@gmail.com>
2023-06-26 09:53:09 -0700

    oijkftjkgdl

--- database/dataBaseChallenges.js
+++ database/dataBaseChallenges.js
@@ -1,4 +1,5 @@
 export const dataBase = {
+    //!                                         Push Up
     pushUp: [
         {
             infoChallenge: { start: '1402/03/25', end: '1402/03/27', period: 3, target: 100 },
@@ -7,12 +8,13 @@ export const dataBase = {
             abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [20, 10, 70] }
         },
         {
-            infoChallenge: { start: '1402/03/03', end: '1402/03/09', period: 7, target: 300 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [] }
+            infoChallenge: { start: '1402/04/03', end: '1402/04/09', period: 7, target: 300 },
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [40, 20] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [50, 100] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [20, 20] }
         },
     ],
+    //!                                         Lesson
     lesson: [
         {
             infoChallenge: { start: '1402/03/25', end: '1402/03/27', period: 5, target: 1000 },
@@ -21,18 +23,19 @@ export const dataBase = {
             abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [130, 140, 225, 180, 100] }
         },
         {
-            infoChallenge: { start: '1402/03/03', end: '1402/03/08', period: 6, target: 1200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [] }
+            infoChallenge: { start: '1402/04/03', end: '1402/04/08', period: 6, target: 1200 },
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [195, 190] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [235, 203] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [185, 195] }
         },
     ],
+    //!                                         Study
     study: [
         {
-            infoChallenge: { start: '1402/03/03', end: '1402/03/16', period: 14, target: 200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [] }
+            infoChallenge: { start: '1402/04/03', end: '1402/04/16', period: 14, target: 200 },
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [12, 0] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [0, 0] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [0, 0] }
         },
     ],
 }
