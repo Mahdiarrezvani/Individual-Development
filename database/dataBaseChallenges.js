5ecee4133727cbfac3f593ec473d9dadb66ab858
mahdiar <mahdiarrezvani59@gmail.com>
2023-06-29 07:39:25 -0700

    aploud new page challenge

--- database/dataBaseChallenges.js
+++ database/dataBaseChallenges.js
@@ -10,15 +10,15 @@ export const dataBase = {
         //?                                  doing  doing  doing
         {
             infoChallenge: { start: '1402/04/03', end: '1402/04/09', period: 7, target: 300 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [40, 20, 50, 40] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [50, 100, 20, 24] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [20, 20, 20, 20] }
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [40, 20, 50, 40, 60] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [50, 100, 20, 24, 20] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [20, 20, 20, 20, 20] }
         },
     ],
     //!                                         Lesson
     lesson: [
         {
-            infoChallenge: { start: '1402/03/25', end: '1402/03/27', period: 5, target: 1000 },
+            infoChallenge: { start: '1402/03/27', end: '1402/03/31', period: 5, target: 1000 },
             mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [170, 205, 295, 95, 240] },
             amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [180, 251, 104, 122, 48] },
             abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [130, 140, 225, 180, 100] }
@@ -26,9 +26,9 @@ export const dataBase = {
         //?                                  doing  doing  doing
         {
             infoChallenge: { start: '1402/04/03', end: '1402/04/08', period: 6, target: 1200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [195, 190, 210, 116] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [235, 203, 66, 200] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [185, 195, 200, 90] }
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [195, 190, 210, 116, 182] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [235, 203, 66, 200, 5] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [185, 195, 200, 90, 225] }
         },
     ],
     //!                                         Study
@@ -36,9 +36,9 @@ export const dataBase = {
         //?                                  doing  doing  doing
         {
             infoChallenge: { start: '1402/04/03', end: '1402/04/16', period: 14, target: 200 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [12, 0, 19, 0] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [0, 0, 15, 15] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [0, 0, 0, 0] }
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [12, 0, 19, 0, 29] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [0, 0, 15, 15, 0] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [0, 0, 0, 0, 0] }
         },
     ],
 }
--- js/challenges.js
+++ js/challenges.js
@@ -1,125 +1,163 @@
 import { dataBase } from "../database/dataBaseChallenges.js";
+import { backHomePage } from "./mixina.js";
+backHomePage();
 // ! متغیر ها
-let title = document.querySelector('.title');
-let containerChallnge = document.querySelector('.container-challnge');
-let backHistoryBtn = document.querySelector('.back-history');
-let informationChallengeElem = document.querySelector('.information-challenge');
-let processChallenges = document.querySelector('.process-challenges');
-let resultChallenge = document.querySelector('.result-challenge');
-let stateChallengeEnd = document.querySelector('.state-challenge-end');
-let challenge, getChallengeDB, getChallengeParams, idChallenge, numberDay;
-let containerBtnChallenge = document.querySelector('.container-btn-challenge');
-containerChallnge.style.display = "none";
-let persons = ['mahdiar', 'amin', 'abbas'];
-getParams()
+let typeChallenge = 'lesson';
+let numberChallenge = 0;
+let arrayChallenge = dataBase[typeChallenge][numberChallenge];
+let numberChallengeElems;
+// 
+let activeElemChallenge = document.querySelector('.active-challenge');
+activeElemChallenge.style.backgroundColor = "#252525";
+// 
+let sectionBtnNumber = document.querySelector('.section-btn-number');
+function createBtn() {
+    sectionBtnNumber.innerHTML = "";
+    let number = 1;
+    let lengthArray = dataBase[typeChallenge];
+    lengthArray.forEach(function () {
+        sectionBtnNumber.insertAdjacentHTML('beforeend', `<p class="number-challenge active-btn${number}">${number}</p>`);
+        number++;;
+    });
+    let activeElemBtn = document.querySelector('.active-btn1');
+    activeElemBtn.style.backgroundColor = "#252525";
+    numberChallengeElems = document.querySelectorAll('.number-challenge');
+}
+createBtn();
+// 
+function bgColorBtns() {
+    let activeBtns = document.querySelectorAll('.number-challenge');
+    activeBtns.forEach(function (e) {
+        e.style.backgroundColor = "transparent";
+    })
+}
+function bgColorBtnsChallenge() {
+    let activeBtnsChallenge = document.querySelectorAll('.btn-asdasdsd');
+    activeBtnsChallenge.forEach(function (e) {
+        e.style.backgroundColor = "transparent";
+    })
+}
+// 
+let challengeBtn = document.querySelectorAll('.btn-asdasdsd');
+function createBtnChallnge() {
+    challengeBtn.forEach(function (elem) {
+        elem.addEventListener('click', function (e) {
+            bgColorBtnsChallenge();
+            e.target.style.backgroundColor = "#252525";
+            typeChallenge = e.target.attributes.type.nodeValue;
+            arrayChallenge = dataBase[typeChallenge][0]
+            createBtn();
+            infoChallenFunc();
+            sumNumbersChallenge();
+            createNumberBtn();
+        });
+    })
+}
+function createNumberBtn() {
+    numberChallengeElems.forEach(function (elem) {
+        elem.addEventListener('click', function (e) {
+            bgColorBtns();
+            e.target.style.backgroundColor = "#252525";
+            numberChallenge = e.target.innerHTML - 1;
+            arrayChallenge = dataBase[typeChallenge][numberChallenge];
+            infoChallenFunc();
+            sumNumbersChallenge();
+        });
+    })
+}
 // ! Functions
-function backHistory() {
-    history.back();
+let infoChallengeElem = document.querySelector('.info-challenge');
+function infoChallenFunc() {
+    infoChallengeElem.innerHTML = '';
+    let infoChallenge = arrayChallenge.infoChallenge;
+    infoChallengeElem.insertAdjacentHTML('beforeend', `
+        <p>start : <span class="value">${infoChallenge.start}</span></p>
+        <p>end : <span class="value">${infoChallenge.end}</span></p>
+        <p>period : <span class="value">${infoChallenge.period} day</span></p>
+        <p>target : <span class="value">${infoChallenge.target}</span></p>`);
 }
-function getParams() {
-    let params = new URLSearchParams(location.search);
-    idChallenge = params.get('id') - 1;
-    getChallengeParams = params.get('challenge');
-    console.log(getChallengeParams);
-    title.innerHTML = getChallengeParams;
-    getChallengeDB = dataBase[getChallengeParams]
-    challenge = getChallengeDB[idChallenge];
-}
-function stateChallen() {
-    let infoChallenge = challenge.infoChallenge;
-    informationChallengeElem.insertAdjacentHTML('beforeend', `
-        <td>${infoChallenge.start}</td>
-        <td>${infoChallenge.end}</td>
-        <td>${infoChallenge.period} day</td>
-        <td>${infoChallenge.target}</td>`);
-}
-function createSectionInfo() {
-    let andis = 0;
-    let day = 0;
-    for (let a = 0; a < numberDay; a++) {
-        day++;
-        processChallenges.insertAdjacentHTML('beforeend', `
-        <tr>
-            <td>${challenge.mahdiar.arrayChallengeProcess[andis]}</td>
-            <td>${challenge.amin.arrayChallengeProcess[andis]}</td>
-            <td>${challenge.abbas.arrayChallengeProcess[andis]}</td>
-            <td>${day}</td>
-        </tr>`);
-        andis++;
-    }
-}
+// let stateChallengeElem = document.querySelector('.state-challenge');
+// function createSectionInfo() {
+//     let andis = 0;
+//     let day = 0;
+//     for (let a = 0; a < 2; a++) {
+//         day++;
+//         stateChallengeElem.insertAdjacentHTML('beforeend', `
+//         <tr>
+//             <td>${arrayChallenge.mahdiar.arrayChallengeProcess[andis]}</td>
+//             <td>${arrayChallenge.amin.arrayChallengeProcess[andis]}</td>
+//             <td>${arrayChallenge.abbas.arrayChallengeProcess[andis]}</td>
+//             <td>${day}</td>
+//         </tr>`);
+//         andis++;
+//     }
+// }
+// createSectionInfo();
 // todo اسم
 function sumNumbersChallenge() {
+    let lengthArray = arrayChallenge.mahdiar.arrayChallengeProcess;
     let andis = 0;
     let mahdiar = 0;
     let amin = 0;
     let abbas = 0;
-    for (let a = 0; a < numberDay; a++) {
+    for (let a = 0; a < lengthArray.length; a++) {
         // todo اسم
-        mahdiar = mahdiar + +challenge.mahdiar.arrayChallengeProcess[andis];
-        amin = amin + +challenge.amin.arrayChallengeProcess[andis];
-        abbas = abbas + +challenge.abbas.arrayChallengeProcess[andis];
+        mahdiar = mahdiar + +arrayChallenge.mahdiar.arrayChallengeProcess[andis];
+        amin = amin + +arrayChallenge.amin.arrayChallengeProcess[andis];
+        abbas = abbas + +arrayChallenge.abbas.arrayChallengeProcess[andis];
         andis++;
     }
-    challenge.mahdiar.result = mahdiar;
-    challenge.amin.result = amin;
-    challenge.abbas.result = abbas;
+    arrayChallenge.mahdiar.result = mahdiar;
+    arrayChallenge.amin.result = amin;
+    arrayChallenge.abbas.result = abbas;
     checkIsSucssesfullChallenge(mahdiar, amin, abbas)
 }
 function checkIsSucssesfullChallenge(mahdiar, amin, abbas) {
-    let target = challenge.infoChallenge.target;
+    let target = arrayChallenge.infoChallenge.target;
     if (mahdiar >= target) {
-        challenge.mahdiar.state = '👍';
+        arrayChallenge.mahdiar.state = '👍';
     }
     if (amin >= target) {
-        challenge.amin.state = '👍';
+        arrayChallenge.amin.state = '👍';
     }
     if (abbas >= target) {
-        challenge.abbas.state = '👍';
+        arrayChallenge.abbas.state = '👍';
     }
     createResult();
 }
+let stateChallengeElem = document.querySelector('.state-challenge');
+let persons = ["mahdiar", 'amin', 'abbas']
 function createResult() {
-    for (let a = 0; a < 3; a++) {
-        resultChallenge.insertAdjacentHTML('beforeend', `
-        <tr>
-        <td>${challenge[persons[a]].name}</td>
-            <td>${challenge[persons[a]].state}</td>
-            <td>${challenge[persons[a]].result}</td>
-        </tr>`);
+    stateChallengeElem.innerHTML = "";
+    for (let a = 0; a < persons.length; a++) {
+        stateChallengeElem.insertAdjacentHTML('beforeend', `
+        <div>
+            <p class="name-person">${arrayChallenge[persons[a]].name}</p>
+            <p>state : <span>${arrayChallenge[persons[a]].state}</span></p>
+            <p>result : ${arrayChallenge[persons[a]].result}</p>
+        </div>`);
     }
 }
-function checkIsEndChallenge() {
-    if (challenge.infoChallenge.period <= challenge.mahdiar.arrayChallengeProcess.length) {
-        stateChallengeEnd.innerHTML = 'end of the challenge';
-        stateChallengeEnd.style.backgroundColor = "#ff1414";
-    } else {
-        stateChallengeEnd.innerHTML = 'doing the challenge'
-    }
-}
-function createBtnChallngeId() {
-    let numberChallenge = 1;
-    getChallengeDB.forEach(function (info) {
-        // let infoChallenge=info.infoChallenge
-        containerBtnChallenge.insertAdjacentHTML('beforeend', `
-        <button><a href="challenges.html?challenge=${getChallengeParams}&id=${numberChallenge}">${numberChallenge}</a></button>`);
-        // todo sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasddas
-        // <button><a href="challenges.html?challenge=${getChallengeParams}&id=${numberChallenge}"><span>${infoChallenge.start}</span><span>${infoChallenge.end}</span><span>${infoChallenge.period}</span><span>${infoChallenge.target}</span></a></button>`);
-        numberChallenge++;
-    })
-}
-// ! Execution Of Function || اجرای تابع ها
-backHistoryBtn.addEventListener('click', backHistory);
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-if (idChallenge == -1) {
-    containerBtnChallenge.style.display = "grid"
-    createBtnChallngeId()
-} else {
-    containerBtnChallenge.style.display = "none"
-    containerChallnge.style.display = "block"
-    numberDay = challenge.mahdiar.arrayChallengeProcess.length;
-    stateChallen();
-    createSectionInfo();
-    sumNumbersChallenge();
-    checkIsEndChallenge();
-}
+createBtnChallnge();
+createNumberBtn();
+infoChallenFunc();
+sumNumbersChallenge();
+// function checkIsEndChallenge() {
+//     if (challenge.infoChallenge.period <= challenge.mahdiar.arrayChallengeProcess.length) {
+//         stateChallengeEnd.innerHTML = 'end of the challenge';
+//         stateChallengeEnd.style.backgroundColor = "#ff1414";
+//     } else {
+//         stateChallengeEnd.innerHTML = 'doing the challenge'
+//     }
+// }
+// function createBtnChallngeId() {
+//     let numberChallenge = 1;
+//     getChallengeDB.forEach(function (info) {
+//         // let infoChallenge=info.infoChallenge
+//         containerBtnChallenge.insertAdjacentHTML('beforeend', `
+//         <button><a href="challenges.html?challenge=${getChallengeParams}&id=${numberChallenge}">${numberChallenge}</a></button>`);
+//         // todo sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasddas
+//         // <button><a href="challenges.html?challenge=${getChallengeParams}&id=${numberChallenge}"><span>${infoChallenge.start}</span><span>${infoChallenge.end}</span><span>${infoChallenge.period}</span><span>${infoChallenge.target}</span></a></button>`);
+//         numberChallenge++;
+//     })
+// }
