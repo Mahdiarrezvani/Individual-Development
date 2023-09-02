e77ed0d3124470641ba04214043b5ae8f0b8f56e
mahdiar <mahdiarrezvani59@gmail.com>
2023-09-01 21:16:21 -0700

    kg

--- js/months.js
+++ js/months.js
@@ -1,29 +1,27 @@
 import { dataBase } from './dataBase.js'
-import {backHomePage} from './mixina.js'
+import { backHomePage } from './mixina.js'
 backHomePage();
 let currentPage = 1;
-let numberShow = 150;
-// let numberShow = 30;
+let numberShow = 30;
 let table = document.querySelector('.table');
 let andis = 0;
-// let numberOfMonth = Math.ceil(dataBase[andis].length / numberShow);
+let numberOfMonth = Math.ceil(dataBase[andis].length / numberShow);
 let btnperson = document.querySelectorAll('.btn-person');
 let activeElem = document.querySelector('.active');
 activeElem.style.backgroundColor = "#252525";
-// 
 let btnPerson = document.querySelectorAll('.btn-person');
 btnPerson.forEach(function (elem) {
     elem.addEventListener('click', function (e) {
         andis = e.target.attributes.andis.nodeValue;
         activeFun();
         e.target.style.backgroundColor = "#252525";
-        createPages()
+        createPages(currentPage);
     });
 });
-function createPages() {
+function createPages(page) {
     table.innerHTML = "";
     let newDataBase = [...dataBase[andis]];
-    let end = numberShow * currentPage;
+    let end = numberShow * page;
     let start = end - numberShow;
     let c = newDataBase.slice(start, end);
     c.forEach(function (info) {
@@ -41,27 +39,27 @@ function createPages() {
     </div>`);
     });
 }
-// function createBtnPage() {
-//     for (let s = 1; s < numberOfMonth + 1; s++) {
-//         btnContainer.insertAdjacentHTML('beforeend', `<button class="btn-page">${s}</button>`);
-//     }
-// }
-// function eventBtns() {
-//     let btnPage = document.querySelectorAll('.btn-page');
-//     btnPage.forEach(function(btn) {
-//         btn.addEventListener('click', function(e) {
-//             table.innerHTML = '';
-//             let page = e.target.innerHTML;
-//             createPages(page);
-//         });
-//     });
-// }
+let btnContainer = document.querySelector('.btn-container');
+function createBtnPage() {
+    for (let s = 1; s < numberOfMonth + 1; s++) {
+        btnContainer.insertAdjacentHTML('beforeend', `<button class="btn-page">${s}</button>`);
+    }
+}
+function eventBtns() {
+    let btnPage = document.querySelectorAll('.btn-page');
+    btnPage.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            table.innerHTML = '';
+            let page = e.target.innerHTML;
+            createPages(page);
+        });
+    });
+}
 function activeFun() {
     btnperson.forEach(function (elem) {
         elem.style.backgroundColor = "transparent";
     })
 }
-createPages();
-// createBtnPage();
-// eventBtns();
-// برای جابه جایی صحفه یه ترنزیشن لازمه
+createPages(currentPage);
+createBtnPage();
+eventBtns();
